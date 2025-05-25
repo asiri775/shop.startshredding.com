214968c04c8e39627ac920cb9106bbcb658e1976
Ishan <ishanpradeepkumara@gmail.com>
2025-05-25 19:41:57 +0530

    Create terms & conditions part

--- project/app/Http/Controllers/ServiceAgreementController.php
+++ project/app/Http/Controllers/ServiceAgreementController.php
@@ -486,7 +486,7 @@ class ServiceAgreementController extends Controller {
         $condition = new TermsAndCondition();
         $condition->title = $request->title;
         $condition->categorie_id = $request->categorie_id;
-        $condition->status = $request->status;
+        $condition->status = $request->status ?? 'inactive';
         $condition->industry_id = $request->industry_id;
         $condition->save();
 
